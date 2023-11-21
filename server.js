1b84be80671776fd813eec753ae98496589c4c40
KazemHe <kazemhe1@gmail.com>
2023-11-22 01:11:10 +0200

    server

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ if (process.env.NODE_ENV === "production") {
 } else {
   corsOptions = {
     origin: [
-      "http://localhost:5173",
-      "http://127.0.0.1:5173",
-      "http://localhost:4000", 
+      "http://localhost:3030",
+      "http://127.0.0.1:3030",
+      "http://localhost:300", 
     ],
     credentials: true,
   };
