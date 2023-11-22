76d64d9e934c6bb3a8ec5806b82a61c942e29639
KazemHe <123581188+KazemHe@users.noreply.github.com>
2023-11-22 10:52:23 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 // Express App Config
 app.use(cookieParser());
 app.use(express.json());
-// let corsOptions;
+let corsOptions;
 
 const io = require("socket.io")(http, {
     cors: {
@@ -19,15 +19,26 @@ const io = require("socket.io")(http, {
 });
 require("./services/socketService")(io);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname,'public')))
- } else {
-  const corsOptions = {
-  origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
-  credentials: true
-  }
-  app.use(cors(corsOptions))
- }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "public")));
+  corsOptions = {
+    origin: "*",
+  };
+} else {
+  corsOptions = {
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "http://127.0.0.1:3030",
+      "http://127.0.0.1:3000",
+      "http://localhost:3030",
+      "http://localhost:3000",
+      "http://localhost:4000", // socket cors
+    ],
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+}
 
 const codeblockRoutes = require("./api/codeBlock/codeBlock.routes");
 
@@ -36,10 +47,11 @@ app.use("/api/codeblock", codeblockRoutes);
 // Make every server-side-route to match the index.html
 // so when requesting http://localhost:3030/index.html/photo/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
+app.get("/**", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 const port = process.env.PORT || 3030;
-app.get('/**', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
-app.listen(port, () => {
- console.log(`App listening on port ${port}!`)
+http.listen(port, () => {
+  console.log("Server is running on port: " + port);
 });
